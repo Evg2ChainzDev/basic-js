4808e8a49dda127d69546ca58e6fda774d0ac9e5
Evg2ChainzDev <88166547+Evg2ChainzDev@users.noreply.github.com>
2022-04-30 19:52:05 +0300

    feat:dreamteam

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (Array.isArray(members) ) {
+    const alp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    members = members.filter(el => typeof el === 'string');
+    let membersTrimmed = members.map(member => member.trim()).map(el => el[0].toUpperCase());
+    console.log(membersTrimmed)
+    // console.log(members)
+    // console.log(members2)
+    let members2sorted = membersTrimmed.sort().join('');
+    // console.log(members2sorted)
+    return members2sorted
+  } else { return false }
 }
 
 module.exports = {
